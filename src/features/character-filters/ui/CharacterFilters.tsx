73595eeb4a30126af73659e7eaeedb094481ca6e
rvadimv <razvodov.vadim@gmail.com>
2026-04-30 07:08:45 +0300

    feat: add character filters

--- src/features/character-filters/ui/CharacterFilters.tsx
+++ src/features/character-filters/ui/CharacterFilters.tsx
@@ -1,26 +1,33 @@
 import type { ChangeEvent } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { updateParams } from '@/shared/lib/updateParams'
+import { useCharacterFilters } from '@/features/character-filters/model/useCharacterFilters'
+
+import s from './CharacterFilters.module.scss'
+
+type FilterKey = 'status' | 'gender'
 
 export const CharacterFilters = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const { status, gender, setFilter } = useCharacterFilters()
 
-  const currentStatus = searchParams.get('status') ?? ''
-
-  const onSelectStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handleSelect = (param: FilterKey) => (e: ChangeEvent<HTMLSelectElement>) => {
     const value = e.currentTarget.value
-
-    updateParams(searchParams, setSearchParams, {
-      page: '1',
-      status: value,
-    })
+    setFilter(param, value)
   }
 
   return (
-    <select value={currentStatus} onChange={onSelectStatus}>
-      <option value="">All</option>
-      <option value="alive">Alive</option>
-      <option value="dead">Dead</option>
-    </select>
+    <div className={s.filtersWrap}>
+      <select value={status} onChange={handleSelect('status')}>
+        <option value="">All</option>
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <select value={gender} onChange={handleSelect('gender')}>
+        <option value="">All</option>
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="genderless">Genderless</option>
+        <option value="unknown">Unknown</option>
+      </select>
+    </div>
   )
 }
--- src/pages/characters-page/lib/useCharactersParams.ts
+++ src/pages/characters-page/lib/useCharactersParams.ts
@@ -1,11 +1,12 @@
 export const getCharactersParams = (searchParams: URLSearchParams) => {
   const name = searchParams.get('name') ?? ''
   const status = searchParams.get('status') ?? ''
+  const gender = searchParams.get('gender') ?? ''
 
   const rawPage = searchParams.get('page')
   const parsedPage = Number(rawPage)
 
   const page = Number.isInteger(parsedPage) && parsedPage > 0 ? parsedPage : 1
 
-  return { name, status, page }
+  return { page, name, status, gender }
 }
